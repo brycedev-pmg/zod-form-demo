9f3d2ad9fa7f8d9b274ab6cd888d7e3ab1c44315
Bryce DeVaughn <bryce.devaughn@pmg.com>
2026-06-30 10:35:18 -0500

    Improved form UI and got the submit working

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,5 +1,16 @@
 import { SignUpFormFields } from "@/types";
 import { Control, Controller } from "react-hook-form";
+import { Checkbox } from "./ui/checkbox";
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from "./ui/field";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 export const SignUpForm = ({
   onSubmit,
@@ -7,51 +18,69 @@ export const SignUpForm = ({
 }: Readonly<SignUpFormProps>) => {
   return (
     <form onSubmit={onSubmit}>
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => {
-          return (
-            <input
-              name={field.name}
-              onBlur={field.onBlur}
-              onChange={field.onChange}
-              value={field.value}
-              type="email"
+      <FieldSet>
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    name={field.name}
+                    onBlur={field.onBlur}
+                    onChange={field.onChange}
+                    value={field.value}
+                    type="email"
+                  />
+                );
+              }}
             />
-          );
-        }}
-      />
-      <Controller
-        name="password"
-        control={control}
-        render={({ field }) => {
-          return (
-            <input
-              name={field.name}
-              onBlur={field.onBlur}
-              onChange={field.onChange}
-              value={field.value}
-              type="password"
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    name={field.name}
+                    onBlur={field.onBlur}
+                    onChange={field.onChange}
+                    value={field.value}
+                    type="password"
+                  />
+                );
+              }}
             />
-          );
-        }}
-      />
-      <Controller
-        name="terms"
-        control={control}
-        render={({ field }) => {
-          return (
-            <input
-              name={field.name}
-              onBlur={field.onBlur}
-              onChange={field.onChange}
-              checked={field.value}
-              type="password"
+          </Field>
+          <Field orientation="horizontal">
+            <Controller
+              name="terms"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Checkbox
+                    name={field.name}
+                    onBlur={field.onBlur}
+                    onChange={field.onChange}
+                    checked={field.value}
+                  />
+                );
+              }}
             />
-          );
-        }}
-      />
+            <FieldContent>
+              <FieldLabel htmlFor="terms">Terms and Conditions</FieldLabel>
+              <FieldDescription>
+                You must accept our terms and conditions. Read more <a>here</a>
+              </FieldDescription>
+            </FieldContent>
+          </Field>
+        </FieldGroup>
+        <Button type="submit">Create account</Button>
+      </FieldSet>
     </form>
   );
 };
